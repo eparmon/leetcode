494936c2ea62942361ff2b8ed52c64878a4a730a
Jauhien Parmon <eparmon@protonmail.com>
2020-06-23 21:42:40 +0300

    Problem 485: Max Consecutive Ones

---
+++ src/main/java/by/eparmon/leetcode/problem485/Solution.java
@@ -0,0 +1,23 @@
+package by.eparmon.leetcode.problem485;
+
+/**
+ * Given a binary array, find the maximum number of consecutive 1s in this array.
+ * https://leetcode.com/problems/max-consecutive-ones/
+ */
+public class Solution {
+
+    public int findMaxConsecutiveOnes(int[] numbers) {
+        int maxRowLength = 0;
+        int currentRowLength = 0;
+        for (int number : numbers) {
+            if (number == 1) {
+                currentRowLength++;
+            } else {
+                maxRowLength = Math.max(maxRowLength, currentRowLength);
+                currentRowLength = 0;
+            }
+        }
+        return Math.max(maxRowLength, currentRowLength);
+    }
+
+}
