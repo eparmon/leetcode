92e181c78d015c32f7b467bc5bf1260f8f14f250
Jauhien Parmon <eparmon@protonmail.com>
2020-06-23 22:36:48 +0300

    Problem 1295: Find Numbers with Even Number of Digits

---
+++ src/test/java/by/eparmon/leetcode/problem1295/SolutionTest.java
@@ -0,0 +1,15 @@
+package by.eparmon.leetcode.problem1295;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(2, solution.findNumbers(new int[]{12,345,2,6,7896}));
+    }
+
+}
