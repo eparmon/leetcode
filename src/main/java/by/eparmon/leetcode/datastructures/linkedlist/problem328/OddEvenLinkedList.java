e7d9dc652de83887e77824ddb23dab89a22342c1
Gene <eparmon@tuta.io>
2022-03-30 15:25:34 +0400

    Problem 328: Odd Even Linked List

--- src/main/java/by/eparmon/leetcode/datastructures/linkedlist/problem328/OddEvenLinkedList.java
+++ src/main/java/by/eparmon/leetcode/datastructures/linkedlist/problem328/OddEvenLinkedList.java
@@ -2,6 +2,18 @@ package by.eparmon.leetcode.datastructures.linkedlist.problem328;
 
 import by.eparmon.leetcode.datastructures.linkedlist.ListNode;
 
+/**
+ * Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even
+ * indices, and return the reordered list.
+ * <p>
+ * The first node is considered odd, and the second node is even, and so on.
+ * <p>
+ * Note that the relative order inside both the even and odd groups should remain as it was in the input.
+ * <p>
+ * You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+ * <p>
+ * <a href="https://leetcode.com/problems/odd-even-linked-list/">Leetcode</a>
+ */
 public class OddEvenLinkedList {
 
     public ListNode solve(ListNode head) {
